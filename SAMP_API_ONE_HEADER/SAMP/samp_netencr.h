32d4c5cd9ece410d4bb6a90ce8ce8fb1eaad4de7
DpO4uLa <49291324+DpO4uLa@users.noreply.github.com>
2021-11-05 20:19:48 +0300

    Add files via upload

---
+++ SAMP_API_ONE_HEADER/SAMP/samp_netencr.h
@@ -0,0 +1,7 @@
+/*
+	Updated to 0.3.7 by P3ti
+*/
+
+char *DumpMem(unsigned char *pAddr, int len);
+extern unsigned char encrBuffer[4092];
+void kyretardizeDatagram(unsigned char *buf, int len, int port, int unk);
